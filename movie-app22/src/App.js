de17128432279fa16881639ffdf73776be4100b4
AnJiIn <aji0719@gmail.com>
2022-02-09 21:13:32 +0900

    6.0

--- movie-app22/src/App.js
+++ movie-app22/src/App.js
@@ -1,10 +1,14 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useState } from 'react'
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h2 className={styles.title}>Hello</h2>
+      <h2 className={styles.title}>{counter}</h2>
+      <button onClick={onClick}>Click me</button>
       <Button text={"continue"} />
     </div>
   );

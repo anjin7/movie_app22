e2fb1454105f656c4138b52f032c1d88eae7f330
AnJiIn <aji0719@gmail.com>
2022-02-10 12:54:05 +0900

    6.1

--- movie-app22/src/App.js
+++ movie-app22/src/App.js
@@ -1,10 +1,17 @@
 import Button from './Button';
 import styles from './App.module.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(() => {
+    console.log("CALL THE API")
+  }, []);
   return (
     <div>
       <h2 className={styles.title}>{counter}</h2>

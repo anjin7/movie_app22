4cc6df2ea466cefbc6247be3a1ad4d50d18a86e8
_An_Jin_ <aji0719@gmail.com>
2022-02-11 22:32:15 +0900

    5.2.1

--- movie-app22/src/App.js
+++ movie-app22/src/App.js
@@ -4,16 +4,26 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("I run only once");
-  };
   useEffect(() => {
     console.log("CALL THE API")
   }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder='Search here...' 
+      />
       <h2 className={styles.title}>{counter}</h2>
       <button onClick={onClick}>Click me</button>
       <Button text={"continue"} />
